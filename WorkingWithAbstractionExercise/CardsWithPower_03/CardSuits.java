2ad7e346d83ada3184a06a0b58f8f24c917d9644
viktorkrustev <viktorkrustev03@abv.bg>
2023-09-12 22:26:59 +0300

    Exercise

---
+++ WorkingWithAbstractionExercise/CardsWithPower_03/CardSuits.java
@@ -0,0 +1,18 @@
+package WorkingWithAbstractionExercise.CardsWithPower_03;
+
+public enum CardSuits {
+    CLUBS(0),
+    DIAMONDS(13),
+    HEARTS(26),
+    SPADES(39);
+
+    int suitPower;
+
+    CardSuits(int suitPower) {
+        this.suitPower = suitPower;
+    }
+
+    public int getSuitPower() {
+        return suitPower;
+    }
+}
