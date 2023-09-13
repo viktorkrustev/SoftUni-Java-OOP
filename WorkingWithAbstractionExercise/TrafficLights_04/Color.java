a8051723910f37fcfd7e1ae4030d46b855a41639
viktorkrustev <viktorkrustev03@abv.bg>
2023-09-13 19:13:03 +0300

    Exercise

---
+++ WorkingWithAbstractionExercise/TrafficLights_04/Color.java
@@ -0,0 +1,7 @@
+package WorkingWithAbstractionExercise.TrafficLights_04;
+
+public enum Color {
+    RED,
+    GREEN,
+    YELLOW;
+}
---
+++ WorkingWithAbstractionExercise/TrafficLights_04/Light.java
@@ -0,0 +1,27 @@
+package WorkingWithAbstractionExercise.TrafficLights_04;
+
+public class Light {
+    private Color color;
+
+    public Light(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void changeColor(){
+        if (this.color == Color.RED){
+            this.color = Color.GREEN;
+        } else if (this.color == Color.YELLOW){
+            this.color = Color.RED;
+        } else if (this.color == Color.GREEN) {
+            this.color = Color.YELLOW;
+        }
+    }
+}
