9b01106af81771ddd4d979d8d62ec048c3ab4e11
viktorkrustev <viktorkrustev03@abv.bg>
2023-09-17 20:28:20 +0300

    Exercise

---
+++ Inheritance - Lab/RandomArrayList_03/RandomArrayList.java
@@ -0,0 +1,11 @@
+package InheritanceLab.RandomArrayList_03;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList extends ArrayList {
+    public Object getRandomElement(){
+        int index = new Random().nextInt(super.size());
+        return super.remove(index);
+    }
+}
