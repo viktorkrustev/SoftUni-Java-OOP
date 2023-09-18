e48ef8d824c11e9f2c24d38155ec9e87bed61fb3
viktorkrustev <viktorkrustev03@abv.bg>
2023-09-18 21:53:19 +0300

    Exercise

---
+++ Inheritance - Exercises/hero/Elf.java
@@ -0,0 +1,8 @@
+package InheritanceExercises.hero;
+
+public class Elf extends Hero{
+
+    public Elf(String username, int level) {
+        super(username, level);
+    }
+}
---
+++ Inheritance - Exercises/hero/Main.java
@@ -0,0 +1,7 @@
+package InheritanceExercises.hero;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
