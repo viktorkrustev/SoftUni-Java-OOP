e3d1f0ea4bddf418bd05aa19d4993883efc5ce7c
viktorkrustev <viktorkrustev03@abv.bg>
2023-09-18 21:53:40 +0300

    Exercise

---
+++ Inheritance - Exercises/NeedForSpeed_04/SportCar.java
@@ -0,0 +1,10 @@
+package InheritanceExercises.NeedForSpeed_04;
+
+public class SportCar extends Car{
+    private final static double DEFAULT_FUEL_CONSUMPTION = 10;
+
+    public SportCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        super.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}
