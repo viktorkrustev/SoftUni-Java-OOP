113413d72bb9d3e4fc0d52e6027e6a35dc0e0d32
viktorkrustev <viktorkrustev03@abv.bg>
2023-09-19 23:09:27 +0300

    Exercise

---
+++ Interfaces and Abstraction - Lab/CarShopExtended_02/Rentable.java
@@ -0,0 +1,7 @@
+package InterfacesAndAbstractionLab.CarShopExtended_02;
+
+public interface Rentable extends Car{
+    Integer getMinRentDay();
+    Double getPricePerDay();
+
+}
---
+++ Interfaces and Abstraction - Lab/CarShopExtended_02/Sellable.java
@@ -0,0 +1,6 @@
+package InterfacesAndAbstractionLab.CarShopExtended_02;
+
+public interface Sellable extends Car{
+    Double getPrice();
+
+}
