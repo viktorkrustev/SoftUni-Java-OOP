7894ba2f851bcfb5d9613b1f96e9423c6b556ae9
viktorkrustev <viktorkrustev03@abv.bg>
2023-09-26 21:19:15 +0300

    Exercise

---
+++ Polymorphism - Lab/Animals/Cat.java
@@ -0,0 +1,14 @@
+package PolymorphismLab.Animals;
+
+public class Cat extends Animal{
+
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+
+    @Override
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s%nMEEOW", this.getName(), this.getFavouriteFood());
+    }
+}
